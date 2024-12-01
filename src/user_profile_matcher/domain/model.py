4a5605b3014920c32cbb0b8846f2b02db594eed3
Ivan Zamula <ivan.zamula@gmail.com>
2024-12-01 23:22:55 +0300

    Initial commit

--- src/user_profile_matcher/domain/model.py
+++ src/user_profile_matcher/domain/model.py
@@ -1,9 +1,63 @@
 from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Devices:
+    id: int
+    model: str
+    carrier: str
+    firmware: str
+
+@dataclass
+class Inventory:
+    cash: int
+    coins: int
+    item_1: int
+    item_34: int
+    item_55: int
+
+@dataclass
+class Clan:
+    id: int
+    name: str
 
 @dataclass
 class PlayerProfile:
     player_id: int
+    credential: str
+    created: datetime
+    modified: datetime
+    last_session: datetime
+    total_spent: int
+    total_refund: int
+    total_transactions: int
+    last_purchase: datetime
+    active_campaigns: list[str]
+    devices: list[Devices]
+    level: int
+    xp: int
+    total_playtime: int
+    country: str
+    language: str
+    birthdate: datetime
+    gender: str
+    clan: Clan
+    _custom_field: str
+
+@dataclass
+class Matcher:
+    level: bool # min 1 max 3
+    has: bool # country items
+    does_not_have: bool # items
 
 @dataclass
 class CampaignData:
-    pass
+    game: str
+    name: str
+    priority: float
+    matchers: Matcher
+    start_date: datetime
+    end_date: datetime
+    enabled: bool
+    last_update: datetime
--- src/user_profile_matcher/entrypoints/flask_app.py
+++ src/user_profile_matcher/entrypoints/flask_app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route("/get_client_config/<player_id>", methods=["GET"])
 def get_client_config(player_id):
-    result = {'1': player_id}
-    result = views.get_player_profile(player_id=player_id)
+    player_profile = views.get_player_profile(player_id=player_id)
+    campaign_data = None
+    if check_matches(player_profile, campaign_data):
+        update_player_profile(player_profile)
+
+    result = player_profile
     if not result:
         return "not found", 404
     return jsonify(result), 200
